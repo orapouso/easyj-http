55c9b64ca12cff983a4e53c03912bac30827165d
rraposo <rraposo@foxnote>
2012-05-08 00:59:58 -0300

    Added LICENSE and some basic javadoc

--- src/main/java/org/easyj/http/RESTHttpClient.java
+++ src/main/java/org/easyj/http/RESTHttpClient.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2009-2012 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.easyj.http;
 
 import java.io.IOException;
@@ -27,7 +43,8 @@ import org.slf4j.LoggerFactory;
  * Class that wraps HttpClient and HttpMethodBase in the same place.
  * It also enables chaining for quick configuration and execution
  *
- * @author rraposo
+ * @author Rafael Raposo
+ * @since 1.0.0
  */
 public class RESTHttpClient {
 
